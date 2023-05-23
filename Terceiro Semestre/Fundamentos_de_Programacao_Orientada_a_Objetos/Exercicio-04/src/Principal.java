d49fa75990f392970bf46ac1c78c94f8cedfcd59
Anderson Richard <richard-r3@hotmail.com>
2023-05-23 17:04:30 -0300

    Atualizacao exercicio 04

--- Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Exercicio-04/src/Principal.java
+++ Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Exercicio-04/src/Principal.java
@@ -1,7 +1,14 @@
 public class Principal {
     public static void main(String[] args) {
         GerenciadorDeElementos gerenciador = new GerenciadorDeElementos();
-        Triangulo tri = new Triangulo(10, 12, 12, 10, 10);
+
+        Circulo cir = new Circulo(5);
+        gerenciador.obterInformacoes(cir);
+
+        Quadrilatero quar = new Quadrilatero(5,4);
+        gerenciador.obterInformacoes(quar);
+
+        Triangulo tri = new Triangulo(10, 12, 12);
 
         gerenciador.obterInformacoes(tri);
     }
--- Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Exercicio-04/src/Triangulo.java
+++ Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Exercicio-04/src/Triangulo.java
@@ -3,7 +3,7 @@ public class Triangulo {
     private double ladoB;
     private double ladoC;
 
-    public Triangulo(double ladoA, double ladoB, double ladoC, int i, int i1) {
+    public Triangulo(double ladoA, double ladoB, double ladoC) {
         this.ladoA = ladoA;
         this.ladoB = ladoB;
         this.ladoC = ladoC;
