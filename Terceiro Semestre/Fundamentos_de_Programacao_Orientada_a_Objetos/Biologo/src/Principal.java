a6e87998fdb8b07facba3f7815fea19b71dcd8cd
Anderson Richard <richard-r3@hotmail.com>
2023-05-24 15:36:14 -0300

    Conteudo da aula de OO e atualização dos arquivos DB

---
+++ Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Biologo/src/Principal.java
@@ -0,0 +1,11 @@
+import java.text.ParseException;
+
+import Models.Animal;
+import Views.InterfacePrincipalView;
+public class Principal {
+    public static void main(String[] args) throws ParseException {
+
+        InterfacePrincipalView view = new InterfacePrincipalView();
+        view.executar();
+    }
+}
