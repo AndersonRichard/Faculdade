47a403e13f219e6f69de71b2cff8e1775c23321d
AndersonRichard <richard-r3@hotmail.com>
2023-05-30 14:01:16 -0300

    Adicionado respostas e correção do exercicio01

--- Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Exercicio01/src/Main.java
+++ Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Exercicio01/src/Main.java
@@ -1,5 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+ /*
+    Lista de exercícios 01
+    1 - Explique com suas palavras o que são paradigmas de programação.
+
+    **Um paradigma pode ser apresentado como um modelo que serve para padronização onde podemos seguir.
+    Desta forma podendo atender determinada necessidade, onde poderá resolver determinado problema.
+    **correção professor: É um padrão de organização e desenvolvimento da linguagem de programação.
+
+    2 - No paradigma de programação Funcional, tudo é baseado em funções. Qual a vantagem que isto traz em relação ao
+    código e a manutenibilidade?
+
+    **Neste paradigma permite que tenhamos vantagens tais como um código limpo desta forma possibilitando mais
+    previsibilidade tendo pouca dependências, possui padrões de projeto permitindo assim mais agilidade na hora de
+    desenvolver um projeto e por fim quanto a manutenibilidade como os código serão mais limpos, padronizados e
+    objetivos fica fácil dar a manutenção necessária e se for o caso implementar melhorias.
+    **correção professor: Imutabilidade.
+
+    3 - Qual(is) paradigma(s) podemos usar desenvolvendo um programa usando Java script?
+
+    **Paradigma Funcional e paradigma baseado em eventos.
+    **correção professor: OO, eventos, procedural e funcional
+
+    4 - Qual o foco da programação orientada a objetos?
+
+    **Tem como foco representar o mundo real no código desta forma objetos possuem características e comportamentos onde
+    os mesmos se relacionam.
+    **correção professor: Abstração do mundo real.
+
+    5 - Cite 02 problemas que a programação orientada a objetos veio resolver e explique o porque isto é um problema.
+
+    **Dificuldade de se manter o código em projetos grandes: na OO o código é melhor estruturado desta forma permitindo
+    maior facilidade para se manter em projetos grandes onde o código será maior. Reuso limitado do código: na OO como
+    o código é melhor estruturado permite que o reuso de código seja feita de forma mais simplificada.
+    **correção professor: Dificuldade em lidar com programas grandes, Dificuldade em reutilizar o código
+    Devido a baixa flexibilidade de organização deste paradigma.
+
+    6 - Como escolher qual paradigma de programação utilizar?
+
+    **Para que seja feita a escolha de determinado paradigma será necessário que o desenvolvedor deverá analisar o
+    cenário no qual irá trabalhar e como resolver determinado problema pois podemos utilizar vários paradigmas para se
+    resolver determinado problema.
+    **correção professor: Depende de diversos fatores, tamanho do problema, da aplicação, da equipe.
+
+    7 - Explique um caso que a programação procedural é melhor que a programação orientada a objetos.
+
+    **No caso a procedural é melhor utilizada em projetos pequenos como uma calculadora onde será fácil a manutenção e em
+    casos de uso do Arduino onde se utiliza a linguagem C.
+    **correção professor: Aplicações simples e menores.
+
+    8 - Qual trabalho a mais precisamos ter quando estamos trabalhando com programação orientada a objetos?
+
+    **Exige mais planejamento e design de código antes da implementação
+    **correção professor: Fazer o design do código e da arquitetura antes de começar a desenvolver.
+
+    */
+
     }
 }
