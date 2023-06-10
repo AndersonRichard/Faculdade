9fd3c8ac5ef7d61bcfdf372b92c8ca9ad9a0b06d
AndersonRichard <richard-r3@hotmail.com>
2023-06-09 22:25:44 -0300

    Adiciona atualização trabalho

--- Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Realiza_Evento/src/main.java
+++ Terceiro Semestre/Fundamentos_de_Programacao_Orientada_a_Objetos/Realiza_Evento/src/main.java
@@ -1,10 +1,9 @@
 import views.Menu;
 
 //Casamento: Um evento especial onde duas pessoas celebram sua união em matrimônio.
+
 public class main {
     public static void main(String[] args) {
-
-
         Menu menu = new Menu();
         menu.menuInicial();
     }
