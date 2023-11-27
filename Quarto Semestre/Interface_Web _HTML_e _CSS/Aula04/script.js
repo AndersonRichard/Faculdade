a7fcb30c8de0c02a06dbbb32c8573a570c3f2242
AndersonRichard <richard-r3@hotmail.com>
2023-11-27 16:00:51 -0300

    Adiciona conteúdo da aula04

---
+++ Quarto Semestre/Interface_Web _HTML_e _CSS/Aula04/script.js
@@ -0,0 +1,8 @@
+function submitForm() {
+    var name = document.getElementById('name').value;
+    var name = document.getElementById('email').value;
+    var name = document.getElementById('message').value;
+
+    alert(name + ' ' + email + ' ' + mensagem);
+}
+
